ffad3c4e1892ec5b4e451a42054465fc462d75d8
RiddleASL <n00212142@iadt.ie>
2023-11-15 16:00:59 +0000

    a

---
+++ Assets/Scripts/Player/playerInventory.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class playerInventory : MonoBehaviour
+{
+    [SerializeField] TextAsset json;
+
+    //Player Stuff
+    [System.Serializable]
+    public class Position{
+        public float x;
+        public float y;
+        public float z;
+    }
+    [System.Serializable]
+    public class Info{
+        public Position position;
+        public int health;
+    }
+
+    // Inventory Stuff
+    [System.Serializable]
+    public class Blocks{
+        public string name;
+        public GameObject block;
+        public int count;
+    }
+    [System.Serializable]
+    public class Inventory{
+        public List<Blocks> blocks;
+    }    
+
+    [System.Serializable]
+    public class PlayerProps{
+        public Inventory inventory;
+        public Info playerInfo;
+    }
+
+    public PlayerProps full = new PlayerProps();
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(json == null){
+            json = Resources.Load<TextAsset>("JSON/playerProps");
+        }
+        full = JsonUtility.FromJson<PlayerProps>(json.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            SaveData();
+        }
+    }
+
+    void SaveData(){
+        string saveData = JsonUtility.ToJson(full);
+        File.WriteAllText(Application.dataPath + "/Scripts/Player/playerProps.json", saveData);
+    }
+}
