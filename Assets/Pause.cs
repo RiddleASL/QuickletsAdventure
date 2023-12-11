71de7224d8092d50d44559a4268c0339cfec816c
Ben Sharkey <n00212320@iadt.ie>
2023-12-11 15:50:29 +0000

    Pause Script completed

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -8,7 +8,29 @@ public class Pause : MonoBehaviour
     public GameObject PauseMenu;
     public GameObject PauseButton;
 
-    public void QuitToMenu(){
+    void Update()
+    {
+        void PauseGame()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+
+        void ResumeGame()   {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+
+    }
+
+    public void QuitToMenu()
+    {
         SceneManager.LoadScene(0);
     }
 }
